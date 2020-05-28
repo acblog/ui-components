4b5aaa36f2ce8b6e1d22c7fb80367b54767479c1
StardustDL <stardustdl@163.com>
2020-05-28 17:11:12 +0800

    move js to body

--- src/AcBlog.UI.Components.Core/wwwroot/component.js
+++ src/AcBlog.UI.Components.Core/wwwroot/component.js
@@ -1,18 +1,15 @@
-// This file is to show how a library package may provide JavaScript interop features
-// wrapped in a .NET API
-
 window.AcBlogUIComponentsCore = {
     loadScript: function (src) {
         var fileref = document.createElement('script');
         fileref.setAttribute("type", "text/javascript");
         fileref.setAttribute("src", src);
-        document.getElementsByTagName("head")[0].appendChild(fileref);
+        document.body.appendChild(fileref);
     },
     loadStyleSheet: function (href) {
         var fileref = document.createElement('link');
         fileref.setAttribute("rel", "stylesheet");
         fileref.setAttribute("type", "text/css");
         fileref.setAttribute("href", href);
-        document.getElementsByTagName("head")[0].appendChild(fileref);
+        document.head.appendChild(fileref);
     }
 };
